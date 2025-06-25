149f5dabcb5298b0deab4fd400ca1a8f46f6b078
Anton Shchukin <a.a.shchukin@gmail.com>
2025-06-25 11:12:33 +0700

    ready

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,75 +1,83 @@
 (function($) {
-
-    /* Select placeholder */
-    function selectPlaceholder($element) {
-        if ($element.val() === 'placeholder') {
-            $element.parent('.input').addClass('input--placeholder-is-chosen');
-        } else {
-            $element.parent('.input').removeClass('input--placeholder-is-chosen');
-        }
-    }
-
-    $('select.input__widget').each(function () {
-        selectPlaceholder($(this));
-    }).on('change', function () {
-        selectPlaceholder($(this));
-    });
-
-    /* Expanding textarea */
-    function expandTextarea($element) {
-        $element.css('height', 'auto');
-        $element.css('height', ($element[0].scrollHeight + 2 * parseInt($element.css('border-width'), 10)) + 'px');
-    }
-
-    $('.input--expandable .input__widget').each(function () {
-        expandTextarea($(this));
-    }).on('input', function () {
-        expandTextarea($(this));
-    });
-
-    /* Error field */
-    $('.input__widget').on('focus', function () {
-        $(this).parents('.input').removeClass('input--error');
-        $(this).parents('.input').nextUntil(':not(.helper--error)').remove();
-    });
-
-
-    /* Input Mask */
-    $('[type="tel"]').inputmask({
-        alias: 'phoneru',
-    });
-
-
-    /* Magnific Popup */
-    $('.mfp-handler').magnificPopup({
-        type: 'inline',
-        removalDelay: 200,
-        showCloseBtn: false
-    });
-
-
-    /* Accordion */
-
     $(document).ready(function() {
+
+        /* Select placeholder */
+        function selectPlaceholder($element) {
+            if ($element.val() === 'placeholder') {
+                $element.parent('.input').addClass('input--placeholder-is-chosen');
+            } else {
+                $element.parent('.input').removeClass('input--placeholder-is-chosen');
+            }
+        }
+
+        $('select.input__widget').each(function () {
+            selectPlaceholder($(this));
+        }).on('change', function () {
+            selectPlaceholder($(this));
+        });
+
+        /* Expanding textarea */
+        function expandTextarea($element) {
+            $element.css('height', 'auto');
+            $element.css('height', ($element[0].scrollHeight + 2 * parseInt($element.css('border-width'), 10)) + 'px');
+        }
+
+        $('.input--expandable .input__widget').each(function () {
+            expandTextarea($(this));
+        }).on('input', function () {
+            expandTextarea($(this));
+        });
+
+        /* Error field */
+        $('.input__widget').on('focus', function () {
+            $(this).parents('.input').removeClass('input--error');
+            $(this).parents('.input').nextUntil(':not(.helper--error)').remove();
+        });
+
+
+        /* Input Mask */
+        $('[type="tel"]').inputmask({
+            alias: 'phoneru',
+        });
+
+
+        /* Magnific Popup */
+        $('.mfp-handler').magnificPopup({
+            type: 'inline',
+            removalDelay: 200,
+            showCloseBtn: false
+        });
+
+
+        /* Accordion */
+
         $('.accordion__handler').on('click', function() {
             var $item = $(this).closest('.accordion__item');
             var $accordion = $item.closest('.accordion');
             var index = $item.index();
 
-            // Toggle expanded class on accordion items
+            // Toggle expanded class and animate accordion items
             $item.addClass('accordion__item--expanded')
-              .siblings('.accordion__item')
-              .removeClass('accordion__item--expanded');
+              .find('.accordion__dropdown')
+              .slideDown(300);
 
-            // If accordion__preview exists, update slide classes
+            $item.siblings('.accordion__item')
+              .removeClass('accordion__item--expanded')
+              .find('.accordion__dropdown')
+              .slideUp(300);
+
+            /* Если в аккордионе есть сайдбар с картинками */
             var $preview = $accordion.find('.accordion__preview');
             if ($preview.length) {
                 $preview.find('.accordion__slide')
                   .removeClass('accordion__slide--current')
+                  .fadeOut(300);
+
+                $preview.find('.accordion__slide')
                   .eq(index)
-                  .addClass('accordion__slide--current');
+                  .addClass('accordion__slide--current')
+                  .fadeIn(300);
             }
         });
     });
-
 })(jQuery);
