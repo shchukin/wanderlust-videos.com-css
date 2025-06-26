38cd5876bc0376e667c275836dd1b5d176a676b3
Anton Shchukin <a.a.shchukin@gmail.com>
2025-06-26 11:31:33 +0700

    accordion faq

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -64,94 +64,71 @@
 
         /* Accordion */
 
-        $('.accordion__handler').on('click', function() {
+        $('.accordion--steps .accordion__handler').on('click', function() {
             const $item = $(this).closest('.accordion__item');
             const $accordion = $item.closest('.accordion');
-            const $accordionBody = $item.closest('.accordion__body');
             const index = $item.index();
-            const allowAllClosed = $accordion.hasClass('accordion--allow-all-closed');
-            const $preview = $accordion.find('.accordion__preview');
+            const $preview = $accordion.find('.accordion__desktop-preview');
             const $header = $('.header');
             const headerHeight = $header.length ? $header.outerHeight() : 0;
-            const slideAnimationTime = 300;
+            const expandingAnimationTime = 300;
 
-            if (allowAllClosed || !isDesktop) {
-                // Logic for accordion where all items can be closed
+            // On desktop one item is always active
+            if (isDesktop) {
+
                 if ($item.hasClass('accordion__item--expanded')) {
-                    // Collapse the current item
+                    return;
+                }
+
+                $item.addClass('accordion__item--expanded')
+                  .find('.accordion__dropdown')
+                  .slideDown(expandingAnimationTime);
+
+                /* Close siblings */
+                $item.siblings('.accordion__item')
+                  .removeClass('accordion__item--expanded')
+                  .find('.accordion__dropdown')
+                  .slideUp(expandingAnimationTime);
+
+                /* Images slideshow */
+                if ($preview.length) {
+                    $preview.find('.accordion__slide')
+                      .removeClass('accordion__slide--current')
+                      .fadeOut(expandingAnimationTime);
+
+                    $preview.find('.accordion__slide')
+                      .eq(index)
+                      .addClass('accordion__slide--current')
+                      .fadeIn(expandingAnimationTime);
+                }
+            } else {
+
+                /* On smartphone logic is more traditional. Images are inside of items now */
+                if ($item.hasClass('accordion__item--expanded')) {
                     $item.removeClass('accordion__item--expanded')
                       .find('.accordion__dropdown')
-                      .slideUp(slideAnimationTime);
+                      .slideUp(expandingAnimationTime);
                 } else {
-                    // Expand the current item and collapse others
+
                     $item.addClass('accordion__item--expanded')
                       .find('.accordion__dropdown')
-                      .slideDown(slideAnimationTime);
+                      .slideDown(expandingAnimationTime);
+
+                    $item.siblings('.accordion__item')
+                      .removeClass('accordion__item--expanded')
+                      .find('.accordion__dropdown')
+                      .slideUp(expandingAnimationTime);
 
                     // Scroll to the item with offset for header
                     // Have to do it after timeout because need to wait until things collapsed to get the final cooridinate
                     if(!isDesktop) {
                         setTimeout(function (){
                             $('html, body').animate({
-                                scrollTop: $item.offset().top + 1 /* p;us one is to hide border */
+                                scrollTop: $item.offset().top + 1 /* plus one is to hide border */
                             }, 200);
-                        }, slideAnimationTime);
+                        }, expandingAnimationTime);
                     }
 
-
-                    $item.siblings('.accordion__item')
-                      .removeClass('accordion__item--expanded')
-                      .find('.accordion__dropdown')
-                      .slideUp(slideAnimationTime);
-
-                    // Update preview if it exists, though unlikely to be used with allowAllClosed
-                    if ($preview.length) {
-                        $preview.find('.accordion__slide')
-                          .removeClass('accordion__slide--current')
-                          .fadeOut(slideAnimationTime);
-
-                        $preview.find('.accordion__slide')
-                          .eq(index)
-                          .addClass('accordion__slide--current')
-                          .fadeIn(slideAnimationTime);
-                    }
-                }
-            } else {
-                // Logic for accordion where one item is always active
-                if ($item.hasClass('accordion__item--expanded')) {
-                    return; // Ignore click on active item
-                }
-
-                // Expand the current item and collapse others
-                $item.addClass('accordion__item--expanded')
-                  .find('.accordion__dropdown')
-                  .slideDown(slideAnimationTime);
-
-                // Scroll to the item with offset for header
-                // Have to do it after timeout because need to wait until things collapsed to get the final cooridinate
-                if(!isDesktop) {
-                    setTimeout(function (){
-                        $('html, body').animate({
-                            scrollTop: $item.offset().top
-                        }, 200);
-                    }, slideAnimationTime);
-                }
-
-                $item.siblings('.accordion__item')
-                  .removeClass('accordion__item--expanded')
-                  .find('.accordion__dropdown')
-                  .slideUp(slideAnimationTime);
-
-                // Update preview if it exists
-                if ($preview.length) {
-                    $preview.find('.accordion__slide')
-                      .removeClass('accordion__slide--current')
-                      .fadeOut(slideAnimationTime);
-
-                    $preview.find('.accordion__slide')
-                      .eq(index)
-                      .addClass('accordion__slide--current')
-                      .fadeIn(slideAnimationTime);
                 }
             }
         });
