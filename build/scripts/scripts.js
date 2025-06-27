c11f4eb41859fb258c7e2f1edc3b2bcb9dab9329
Anton Shchukin <a.a.shchukin@gmail.com>
2025-06-27 11:05:05 +0700

    ignore and build

---
+++ build/scripts/scripts.js
@@ -0,0 +1,177 @@
+(function($) {
+    $(document).ready(function() {
+
+        /* Global constants  */
+
+        let isDesktop;
+        let isMobile;
+
+        function initGlobalConstant() {
+            isDesktop = window.matchMedia("(min-width: 740px)").matches;
+            isMobile = !isDesktop;
+        }
+
+        initGlobalConstant();
+        window.addEventListener('resize', initGlobalConstant);
+
+
+
+        /* Select placeholder */
+        function selectPlaceholder($element) {
+            if ($element.val() === 'placeholder') {
+                $element.parent('.input').addClass('input--placeholder-is-chosen');
+            } else {
+                $element.parent('.input').removeClass('input--placeholder-is-chosen');
+            }
+        }
+
+        $('select.input__widget').each(function () {
+            selectPlaceholder($(this));
+        }).on('change', function () {
+            selectPlaceholder($(this));
+        });
+
+        /* Expanding textarea */
+        function expandTextarea($element) {
+            $element.css('height', 'auto');
+            $element.css('height', ($element[0].scrollHeight + 2 * parseInt($element.css('border-width'), 10)) + 'px');
+        }
+
+        $('.input--expandable .input__widget').each(function () {
+            expandTextarea($(this));
+        }).on('input', function () {
+            expandTextarea($(this));
+        });
+
+        /* Error field */
+        $('.input__widget').on('focus', function () {
+            $(this).parents('.input').removeClass('input--error');
+            $(this).parents('.input').nextUntil(':not(.helper--error)').remove();
+        });
+
+
+        /* Input Mask */
+        $('[type="tel"]').inputmask({
+            alias: 'phoneru',
+        });
+
+
+        /* Magnific Popup */
+        $('.mfp-handler').magnificPopup({
+            type: 'inline',
+            removalDelay: 200,
+            showCloseBtn: false
+        });
+
+
+        /* Accordion */
+
+        $('.accordion__handler').on('click', function() {
+            const $item = $(this).closest('.accordion__item');
+            const $accordion = $item.closest('.accordion');
+            const index = $item.index();
+            const $preview = $accordion.find('.accordion__desktop-preview');
+            const $header = $('.header');
+            const headerHeight = $header.length ? $header.outerHeight() : 0;
+            const expandingAnimationTime = 300;
+
+            /* On smartphone, all the accordions behaves the same.
+             * On desktop default accordion also behaves the same but .accordion--steps and .accordion--faq are exception.
+             * Their behaviour is written inside "else" down below
+             */
+            if (isMobile || (!$accordion.hasClass('accordion--steps') && !$accordion.hasClass('accordion--faq'))) {
+
+                /* This is pretty traditional logic: opening one item closes the others.
+                 * Closing the current item is allowed:
+                 */
+
+                /* Close the current item if user want that */
+                if ($item.hasClass('accordion__item--expanded')) {
+                    $item.removeClass('accordion__item--expanded')
+                      .find('.accordion__dropdown')
+                      .slideUp(expandingAnimationTime);
+                    return;
+                }
+
+                /* Expand self */
+                $item.addClass('accordion__item--expanded')
+                  .find('.accordion__dropdown')
+                  .slideDown(expandingAnimationTime);
+
+                /* Close siblings */
+                $item.siblings('.accordion__item')
+                  .removeClass('accordion__item--expanded')
+                  .find('.accordion__dropdown')
+                  .slideUp(expandingAnimationTime);
+
+                /* Scroll document the way that currently open question will be on top of the screen
+                 * This is necessary in the case if the item above is closing moving newly open question
+                 * behind the screen. Note that to calculate coordinates correctly you have to run it
+                 * after everything stop moving.
+                 */
+                if(isMobile) {
+                    setTimeout(function (){
+                        $('html, body').animate({
+                            scrollTop: $item.offset().top + 1 - headerHeight /* plus one is to hide the border behind the screen */
+                        }, 200);
+                    }, expandingAnimationTime);
+                }
+            }
+
+            /* Special behavior for .accordion--steps (desktop only) */
+            else if ($accordion.hasClass('accordion--steps') && isDesktop) {
+
+                /* Can't close current item because whole section, including slideshow will collapse */
+                if ($item.hasClass('accordion__item--expanded')) {
+                    return;
+                }
+
+                /* Open itself */
+                $item.addClass('accordion__item--expanded')
+                  .find('.accordion__dropdown')
+                  .slideDown(expandingAnimationTime);
+
+                /* Close siblings */
+                $item.siblings('.accordion__item')
+                  .removeClass('accordion__item--expanded')
+                  .find('.accordion__dropdown')
+                  .slideUp(expandingAnimationTime);
+
+                /* Sync with images slideshow */
+                if ($preview.length) {
+                    $preview.find('.accordion__slide')
+                      .removeClass('accordion__slide--current')
+                      .fadeOut(expandingAnimationTime);
+
+                    $preview.find('.accordion__slide')
+                      .eq(index)
+                      .addClass('accordion__slide--current')
+                      .fadeIn(expandingAnimationTime);
+                }
+            }
+
+            /* Special behavior for accordion--faq (desktop only) */
+            else if ($accordion.hasClass('accordion--faq') && isDesktop) {
+
+                if ($item.hasClass('accordion__item--expanded')) {
+                    $item.removeClass('accordion__item--expanded')
+                      .find('.accordion__dropdown')
+                      .slideUp(expandingAnimationTime);
+                } else {
+                    $item.addClass('accordion__item--expanded')
+                      .find('.accordion__dropdown')
+                      .slideDown(expandingAnimationTime);
+                }
+            }
+        });
+
+
+        /* Team: add container--wid if 11+ members */
+
+        const $teamList = $('.team__list');
+        if ($teamList.find('.team__member').length >= 11) {
+            $teamList.closest('.container').addClass('container--wide');
+        }
+
+    });
+})(jQuery);
