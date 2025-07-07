3a5637d2b9d85ec01d665c9de434c42dc7d73cf2
Anton Shchukin <a.a.shchukin@gmail.com>
2025-07-07 17:13:23 +0700

    splide

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -221,5 +221,14 @@
             }
         });
 
+
+        /* Splide */
+
+        new Splide( '.splide--init-tools', {
+            perPage: 3,
+            gap: 30,
+            arrowPath: 'M22 26L20.6 24.55L24.15 21H12V19H24.15L20.6 15.45L22 14L28 20L22 26Z'
+        } ).mount();
+
     });
 })(jQuery);
