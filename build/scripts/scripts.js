4eb19485e7ea9cb681c3c28db07e59b38eebaf07
Anton Shchukin <a.a.shchukin@gmail.com>
2025-07-24 15:08:14 +0700

    build

--- build/scripts/scripts.js
+++ build/scripts/scripts.js
@@ -205,41 +205,78 @@
 
         /* Splide */
 
-        new Splide( '.splide--portfolio', {
-            perPage: 3,
-            gap: 30,
-            pagination: false,
-            breakpoints: {
-                740: {
-                    perPage: 1,
-                    pagination: true
-                },
-            }
-        } ).mount();
+        if( $('.splide').length ) {
 
-        new Splide( '.splide--tools', {
-            perPage: 4,
-            gap: 30,
-            pagination: false,
-            breakpoints: {
-                740: {
-                    perPage: 1,
-                    pagination: true
-                },
-            }
-        } ).mount();
+            new Splide( '.splide--portfolio', {
+                perPage: 3,
+                gap: 30,
+                pagination: false,
+                breakpoints: {
+                    740: {
+                        perPage: 1,
+                        pagination: true
+                    },
+                }
+            } ).mount();
 
-        new Splide( '.splide--love', {
-            perPage: 4,
-            gap: 30,
-            pagination: false,
-            breakpoints: {
-                740: {
-                    perPage: 1,
-                    pagination: true
-                },
+            new Splide( '.splide--tools', {
+                perPage: 4,
+                gap: 30,
+                pagination: false,
+                breakpoints: {
+                    740: {
+                        perPage: 1,
+                        pagination: true
+                    },
+                }
+            } ).mount();
+
+            new Splide( '.splide--love', {
+                perPage: 4,
+                gap: 30,
+                pagination: false,
+                breakpoints: {
+                    740: {
+                        perPage: 1,
+                        pagination: true
+                    },
+                }
+            } ).mount();
+
+        }
+
+        /* Select placeholder */
+        function selectPlaceholder($element) {
+            if ($element.val() === 'placeholder') {
+                $element.parent('.input').addClass('input--placeholder-is-chosen');
+            } else {
+                $element.parent('.input').removeClass('input--placeholder-is-chosen');
             }
-        } ).mount();
+        }
+
+        $('select.input__widget').each(function () {
+            selectPlaceholder($(this));
+        }).on('change', function () {
+            selectPlaceholder($(this));
+        });
+
+        /* Expanding textarea */
+        function expandTextarea($element) {
+            $element.css('height', 'auto');
+            $element.css('height', ($element[0].scrollHeight + 2 * parseInt($element.css('border-width'), 10)) + 'px');
+        }
+
+        $('.input--expandable .input__widget').each(function () {
+            expandTextarea($(this));
+        }).on('input', function () {
+            expandTextarea($(this));
+        });
+
+        /* Error field */
+        $('.input__widget').on('focus', function () {
+            $(this).parents('.input').removeClass('input--error');
+            $(this).parents('.input').nextUntil(':not(.helper--error)').remove();
+        });
 
     });
 })(jQuery);
